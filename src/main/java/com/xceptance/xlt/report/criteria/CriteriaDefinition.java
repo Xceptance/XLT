8588a0862ff7ff2f99d67a0892bf3d85a4f2d0f3
Joerg Werner <4639399+jowerner@users.noreply.github.com>
2020-04-21 14:39:01 +0200

    #23: Add Apache license header to source files - make copyright configurable in pom.xml

--- src/main/java/com/xceptance/xlt/report/criteria/CriteriaDefinition.java
+++ src/main/java/com/xceptance/xlt/report/criteria/CriteriaDefinition.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.xceptance.xlt.report.criteria;
 
 import java.util.ArrayList;
