d1b2bd470b45ce5e55c0f2760032549a4160e793
Joerg Werner <4639399+jowerner@users.noreply.github.com>
2020-09-02 12:08:29 +0200

    #50: Invalid characters break XML transformation

--- src/main/java/com/xceptance/xlt/report/XmlReportGenerator.java
+++ src/main/java/com/xceptance/xlt/report/XmlReportGenerator.java
@@ -19,29 +19,35 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.text.StringEscapeUtils;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.basic.DateConverter;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.naming.NameCoder;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
 import com.xceptance.xlt.api.report.ReportCreator;
 import com.xceptance.xlt.common.XltConstants;
 
 /**
  * Load test report generator.
- * 
+ *
  * @author Jörg Werner (Xceptance Software Technologies GmbH)
  */
 public class XmlReportGenerator
 {
     private static final Log LOG = LogFactory.getLog(XmlReportGenerator.class);
 
-    private final List<ReportCreator> processors = new ArrayList<ReportCreator>();
+    private final List<ReportCreator> processors = new ArrayList<>();
 
     public void createReport(final File xmlFile) throws IOException
     {
@@ -92,13 +98,46 @@ public class XmlReportGenerator
         {
             osw.write(XltConstants.XML_HEADER);
 
-            final XStream xstream = new XStream(new DomDriver());
+            final XStream xstream = new XStream(new SanitizingDomDriver());
             xstream.autodetectAnnotations(true);
             xstream.registerConverter(new DateConverter(TimeZone.getDefault()));
             xstream.aliasSystemAttribute(null, "class");
             xstream.setMode(XStream.NO_REFERENCES);
 
             xstream.toXML(testReport, osw);
+        }
+    }
+
+    /**
+     * A custom {@link DomDriver} that uses a {@link SanitizingWriter} to write an XML file.
+     */
+    private static class SanitizingDomDriver extends DomDriver
+    {
+        @Override
+        public HierarchicalStreamWriter createWriter(final Writer out)
+        {
+            return new SanitizingWriter(out, getNameCoder());
+        }
+    }
+
+    /**
+     * A custom {@link PrettyPrintWriter} that silently removes invalid XML 1.0 characters when writing text nodes.
+     */
+    private static class SanitizingWriter extends PrettyPrintWriter
+    {
+        public SanitizingWriter(final Writer writer, final NameCoder nameCoder)
+        {
+            super(writer, nameCoder);
+        }
+
+        @Override
+        protected void writeText(final QuickWriter writer, final String text)
+        {
+            // escape special chars and remove invalid chars
+            final String sanitizedText = StringEscapeUtils.escapeXml10(text);
+
+            // don't call super.writeText() as this would escape the already escaped chars once more
+            writer.write(sanitizedText);
         }
     }
 }
