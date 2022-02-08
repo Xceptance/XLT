1f06b2535b149375b9b8ab23e48fdfac788f3ac1
Joerg Werner <4639399+jowerner@users.noreply.github.com>
2022-02-08 14:24:50 +0100

    imported HtmlUnit 2.58.0

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/Window.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/Window.java
@@ -75,9 +75,9 @@ import com.gargoylesoftware.htmlunit.html.HtmlObject;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
+import com.gargoylesoftware.htmlunit.javascript.HtmlUnitScriptable;
 import com.gargoylesoftware.htmlunit.javascript.JavaScriptEngine;
 import com.gargoylesoftware.htmlunit.javascript.PostponedAction;
-import com.gargoylesoftware.htmlunit.javascript.SimpleScriptable;
 import com.gargoylesoftware.htmlunit.javascript.configuration.JsxClass;
 import com.gargoylesoftware.htmlunit.javascript.configuration.JsxConstant;
 import com.gargoylesoftware.htmlunit.javascript.configuration.JsxConstructor;
@@ -223,7 +223,7 @@ public class Window extends EventTarget implements WindowOrWorkerGlobalScope, Fu
      * @return the prototype object corresponding to the specified class inside the specified scope
      */
     @Override
-    public Scriptable getPrototype(final Class<? extends SimpleScriptable> jsClass) {
+    public Scriptable getPrototype(final Class<? extends HtmlUnitScriptable> jsClass) {
         return prototypes_.get(jsClass);
     }
 
@@ -839,7 +839,7 @@ public class Window extends EventTarget implements WindowOrWorkerGlobalScope, Fu
             final HtmlPage htmlPage = (HtmlPage) enclosedPage;
 
             // Windows don't have corresponding DomNodes so set the domNode
-            // variable to be the page. If this isn't set then SimpleScriptable.get()
+            // variable to be the page. If this isn't set then HtmlUnitScriptable.get()
             // won't work properly
             setDomNode(htmlPage);
             clearEventListenersContainer();
