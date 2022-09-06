55f8f09f01ca566d10fde40339709723381b4c7f
Jörg Werner <4639399+jowerner@users.noreply.github.com>
2022-09-06 14:50:12 +0200

    #258: Request body no longer shown on the Request Information tab (#259)
    * ensure request body is shown
    * fixed a design glitch when showing request body/post parameters
    * minor fixes

--- resultbrowser/src/js/resultbrowser.js
+++ resultbrowser/src/js/resultbrowser.js
@@ -76,7 +76,7 @@
 
     function setText(element, text) {
         if (element) {
-            element.innerText = text;
+            element.innerText = text || "";
         }
     }
 
@@ -514,7 +514,7 @@
             forEachElement(actionlist.querySelectorAll(".active"), (el) => el.classList.remove("active"));
             element.classList.add("active");
 
-            hide(getElementById("errorMessage"))
+            hide(getElementById("errorMessage"));
 
             queryFirst("#jsonViewerActions .search").value = "";
 
@@ -612,7 +612,7 @@
             if (bodyRaw.length > 0) {
                 // request body
                 setText(requestBodySmall.querySelector("textarea"), bodyRaw);
-                hide(requestBodySmall);
+                show(requestBodySmall);
                 hide(postRequestParam);
             }
             else {
