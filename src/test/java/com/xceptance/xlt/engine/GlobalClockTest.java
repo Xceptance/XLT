86b518fec66701459491f975aa141045419cd5de
Rene Schwietzke <r.schwietzke@xceptance.net>
2022-10-02 17:53:42 +0200

    ClockTest renamed

---
+++ src/test/java/com/xceptance/xlt/engine/GlobalClockTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2005-2022 Xceptance Software Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xceptance.xlt.engine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.xceptance.xlt.api.engine.GlobalClock;
+
+public class GlobalClockTest
+{
+    /*
+     * We have a default system clock, make sure this installed
+     * We cannot really test that reliably because someone else might have
+     * used the clock in this VM already
+     */
+    @Test
+    public final void base()
+    {
+        GlobalClock.reset();
+
+        assertEquals("java.time.Clock$SystemClock", GlobalClock.get().getClass().getName());
+        assertEquals(0L, GlobalClock.offsetMillis());
+    }
+
+    /*
+     * We have a default system clock, make sure this installed
+     */
+    @Test
+    public final void fixed() throws InterruptedException
+    {
+        GlobalClock.reset();
+        GlobalClock.installFixed(1010L);
+
+        assertEquals(1010L, GlobalClock.millis());
+        Thread.sleep(676L);
+        assertEquals(1010L, GlobalClock.millis());
+    }
+
+    /*
+     * We have a default system clock, make sure this installed
+     */
+    @Test
+    public final void offset() throws InterruptedException
+    {
+        GlobalClock.reset();
+        GlobalClock.installWithOffset(10000L);
+
+        assertEquals(10000L, GlobalClock.offsetMillis());
+
+        var t1 = GlobalClock.millis();
+        var d1 = Math.abs(t1 - System.currentTimeMillis());
+        assertTrue(d1 <= 10005 && d1 >= 10000);
+
+        Thread.sleep(987L);
+
+        var t2 = GlobalClock.millis();
+        var d2 = Math.abs(t2 - System.currentTimeMillis());
+        assertTrue(d2 <= 10005 && d2 >= 10000);
+
+        var t = t2 - t1;
+        assertTrue(t >= 987);
+    }
+
+    /*
+     * We have a default system clock, make sure this installed
+     */
+    @Test
+    public final void reset()
+    {
+        GlobalClock.installFixed(1000L);
+        assertEquals("java.time.Clock$FixedClock", GlobalClock.get().getClass().getName());
+
+        GlobalClock.reset();
+        assertEquals("java.time.Clock$SystemClock", GlobalClock.get().getClass().getName());
+        assertEquals(0L, GlobalClock.offsetMillis());
+
+        GlobalClock.installWithOffset(2000L);
+        assertEquals("java.time.Clock$OffsetClock", GlobalClock.get().getClass().getName());
+        assertEquals(2000L, GlobalClock.offsetMillis());
+
+        GlobalClock.reset();
+        assertEquals("java.time.Clock$SystemClock", GlobalClock.get().getClass().getName());
+        assertEquals(0L, GlobalClock.offsetMillis());
+    }
+}
