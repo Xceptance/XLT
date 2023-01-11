e5328dfb33e2c6f2d9d06da83bd28a94f052e053
Rene Schwietzke <r.schwietzke@xceptance.net>
2023-01-11 22:02:43 +0100

    Comment, header

--- src/main/java/com/xceptance/common/lang/ParseNumbers.java
+++ src/main/java/com/xceptance/common/lang/ParseNumbers.java
@@ -19,10 +19,11 @@ import java.util.Optional;
 
 /**
  * This is a small helper class for parsing char sequences and converting them into int, long, and double. This implementation is optimized for
- * speed not functionality. It is only able to parse plain numbers with base 10, e.g. 100828171. In case of parsing problems we will fall
+ * speed not functionality. It is only able to parse plain numbers with base 10, e.g. 100828171. In case of parsing problems it will fall
  * back to the JDK but will lose the speed advantage of course.
  *
  * @author René Schwietzke
+ * @since 7.0.0
  */
 public final class ParseNumbers
 {
