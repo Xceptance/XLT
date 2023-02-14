ec3d9b682c68a42bd632d243470402762f865f2b
Julia Schneider <j.schneider@xceptance.net>
2023-02-14 11:54:32 +0100

    collect IP data from timers

---
+++ src/main/java/com/xceptance/xlt/report/providers/IpReportProvider.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2005-2023 Xceptance Software Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xceptance.xlt.report.providers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.xceptance.xlt.api.engine.Data;
+import com.xceptance.xlt.api.engine.RequestData;
+import com.xceptance.xlt.api.report.AbstractReportProvider;
+
+/**
+ * Provides basic statistics for the IP addresses visited during the test.
+ */
+public class IpReportProvider extends AbstractReportProvider
+{
+    /**
+     * A mapping from host names to their corresponding {@link IpReport} objects.
+     */
+    private final Map<String, IpReport> ipReports = new HashMap<String, IpReport>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object createReportFragment()
+    {
+        final IpsReport report = new IpsReport();
+
+        report.ips = new ArrayList<IpReport>(ipReports.values());
+
+        return report;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void processDataRecord(final Data data)
+    {
+        if (data instanceof RequestData)
+        {
+            final RequestData reqData = (RequestData) data;
+            
+            String[] ips = reqData.getIpAddresses();
+            for (int i = 0; i < ips.length; i++)
+            {
+                IpReport ipReport = ipReports.get(ips[i]);
+                if (ipReport == null)
+                {
+                    ipReport = new IpReport();
+                    ipReport.ip = ips[i];
+
+                    ipReports.put(ips[i], ipReport);
+                }
+
+                // update the statistics
+                ipReport.count++;
+            }
+
+            // determine the host name
+            /*String hostName;
+            final String url = reqData.getUrl();
+            if (StringUtils.isBlank(url))
+            {
+                hostName = UNKNOWN_HOST;
+            }
+            else
+            {
+                hostName = extractHostNameFromUrl(url);
+            }*/
+        }
+    }
+
+    /*private String extractHostNameFromUrl(final String url)
+    {
+        String tmp = url;
+
+        // strip protocol if present
+        final int startIndex = tmp.indexOf("://");
+        if (startIndex != -1)
+        {
+            tmp = StringUtils.substring(tmp, startIndex + 3);
+        }
+
+        // strip path/query/fragment if present (whatever comes first)
+        final int endIndex = StringUtils.indexOfAny(tmp, "/?#");
+        if (endIndex != -1)
+        {
+            tmp = StringUtils.substring(tmp, 0, endIndex);
+        }
+
+        return tmp;
+    }*/
+}
