05857994893df74ee26cdcd81879c60ea0fb6668
Rene Schwietzke <r.schwietzke@xceptance.com>
2025-08-05 21:55:48 +0200

    Externalizes merge rule processing for better testing and also for shorter code including less parameters to pass.

--- src/main/java/com/xceptance/xlt/report/DataParserThread.java
+++ src/main/java/com/xceptance/xlt/report/DataParserThread.java
@@ -33,15 +33,14 @@ import com.xceptance.xlt.api.engine.TransactionData;
 import com.xceptance.xlt.api.report.PostProcessedDataContainer;
 import com.xceptance.xlt.api.util.SimpleArrayList;
 import com.xceptance.xlt.api.util.XltCharBuffer;
-import com.xceptance.xlt.report.mergerules.RequestProcessingRule;
-import com.xceptance.xlt.report.mergerules.RequestProcessingRule.ReturnState;
+import com.xceptance.xlt.report.mergerules.RequestProcessing;
 import com.zaxxer.sparsebits.SparseBitSet;
 
 import it.unimi.dsi.util.FastRandom;
 
 /**
- * Parses lines to data records and performs any data record preprocessing that can be done in parallel. Preprocessing
- * also includes executing request merge rules.
+ * Parses lines to data records and performs any data record preprocessing that can 
+ * be done in parallel. Preprocessing also includes executing request merge rules.
  */
 class DataParserThread implements Runnable
 {
@@ -81,12 +80,6 @@ class DataParserThread implements Runnable
     private final ReportGeneratorConfiguration config;
 
     /**
-     * The request processing rules for this thread. Each parser thread gets its own copy of the rule set. This way,
-     * there is no shared state between threads, hence we can more efficiently cache and process stuff.
-     */
-    private final List<RequestProcessingRule> requestProcessingRules;
-
-    /**
      * Constructor.
      *
      * @param dispatcher
@@ -100,7 +93,8 @@ class DataParserThread implements Runnable
      * @param config
      *            the report generator settings
      */
-    public DataParserThread(final Dispatcher dispatcher, final DataRecordFactory dataRecordFactory, final long fromTime, final long toTime,
+    public DataParserThread(final Dispatcher dispatcher, final DataRecordFactory dataRecordFactory, 
+                            final long fromTime, final long toTime,
                             final ReportGeneratorConfiguration config)
     {
         this.dataRecordFactory = dataRecordFactory;
@@ -108,8 +102,6 @@ class DataParserThread implements Runnable
         this.toTime = toTime;
         this.dispatcher = dispatcher;
         this.config = config;
-
-        requestProcessingRules = config.getRequestProcessingRules();
     }
 
     /**
@@ -118,8 +110,6 @@ class DataParserThread implements Runnable
     @Override
     public void run()
     {
-        final boolean removeIndexes = config.getRemoveIndexesFromRequestNames();
-
         final double SAMPLELIMIT = 1 / ((double) config.dataSampleFactor);
         final int SAMPLEFACTOR = config.dataSampleFactor;
 
@@ -132,6 +122,10 @@ class DataParserThread implements Runnable
 
         // make the list large enough so it does not grow, we reuse it anyway
         final SimpleArrayList<XltCharBuffer> csvParseResultBuffer = new SimpleArrayList<>(50);
+
+        // our request processing, this is move away from here to test it better
+        final RequestProcessing requestProcessing = new RequestProcessing(config.getRequestProcessingRules(),
+                                                                          config.getRemoveIndexesFromRequestNames());
 
         while (true)
         {
@@ -228,13 +222,17 @@ class DataParserThread implements Runnable
                         continue;
                     }
 
+                    // set general fields
+                    data.setAgentName(agentName);
+                    data.setTransactionName(testCaseName);
+                    
                     // let's see if this data requires post processing aka filtering/merging
-                    data = applyDataAdjustments(data, agentName, testCaseName, userNumber, collectActionNames, chunk, adjustTimerName);
+                    data = applyDataAdjustments(data, userNumber, collectActionNames, chunk, adjustTimerName);
 
                     // if this is request, filter it aka apply merge rules
                     if (data instanceof RequestData)
                     {
-                        final RequestData result = postprocess((RequestData) data, requestProcessingRules, removeIndexes);
+                        final RequestData result = requestProcessing.postprocess((RequestData) data);
                         if (result != null)
                         {
                             postProcessedData.add(result);
@@ -262,13 +260,11 @@ class DataParserThread implements Runnable
         }
     }
 
-    private Data applyDataAdjustments(final Data data, final String agentName, final String testCaseName, final String userNumber,
-                                      final boolean collectActionNames, final DataChunk lineChunk, boolean adjustTimerName)
+    private Data applyDataAdjustments(final Data data, 
+                                      final String userNumber,
+                                      final boolean collectActionNames, final DataChunk lineChunk, 
+                                      final boolean adjustTimerName)
     {
-        // set general fields
-        data.setAgentName(agentName);
-        data.setTransactionName(testCaseName);
-
         // set special fields / special handling
         if (data instanceof TransactionData)
         {
@@ -291,78 +287,5 @@ class DataParserThread implements Runnable
         }
 
         return data;
-
-    }
-
-    /**
-     * Processes a request according to the configured request processing rules. Currently, this means renaming or
-     * discarding requests.
-     *
-     * @param requestData
-     *            the request data record
-     * @param requestProcessingRules
-     *            the rules to apply
-     * @param removeIndexesFromRequestNames
-     *            in case we want to clean the name too
-     * @return the processed request data record, or <code>null</code> if the data record is to be discarded
-     */
-    private RequestData postprocess(final RequestData requestData, final List<RequestProcessingRule> requestProcessingRules,
-                                    final boolean removeIndexesFromRequestNames)
-    {
-        // fix up the name first (Product.1.2 -> Product) if so configured
-        // this can likely live in RequestData and act on XltCharBuffer instead String
-        if (removeIndexesFromRequestNames)
-        {
-            // @TODO Chance for more performance here
-            String requestName = requestData.getName();
-
-            final int firstDotPos = requestName.indexOf(".");
-            if (firstDotPos > 0)
-            {
-                requestName = requestName.substring(0, firstDotPos);
-                requestData.setName(requestName);
-            }
-        }
-
-        // remember the original name so we can restore it in case request processing fails
-        final String originalName = requestData.getName();
-
-        // execute all processing rules one after the other until processing is complete
-        final int size = requestProcessingRules.size();
-        for (int i = 0; i < size; i++)
-        {
-            final RequestProcessingRule requestProcessingRule = requestProcessingRules.get(i);
-
-            try
-            {
-                // request data comes back indirectly modified if needed
-                final ReturnState state = requestProcessingRule.process(requestData);
-                if (state == ReturnState.DROP)
-                {
-                    return null;
-                }
-                else if (state == ReturnState.STOP)
-                {
-                    break;
-                }
-            }
-            catch (final Throwable t)
-            {
-                final String msg = String.format("Failed to apply request merge rule: %s\n%s", requestProcessingRule, t);
-                LOG.error(msg);
-
-                // restore the request's original name
-                requestData.setName(originalName);
-
-                break;
-            }
-        }
-
-        // ok, we processed all rules for this dataset, get us the final hashcode for the name, because we need that
-        // later
-        // here the cache is likely still hot, so this is less expensive
-        requestData.getName().hashCode();
-
-        return requestData;
     }
 }
