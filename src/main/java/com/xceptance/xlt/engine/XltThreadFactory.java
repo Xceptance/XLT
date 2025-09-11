4d2e6f64b23430393987b32b7355227efa5e1f32
Joerg Werner <4639399+jowerner@users.noreply.github.com>
2025-09-11 16:20:31 +0200

    update to Java 21

--- src/main/java/com/xceptance/xlt/engine/XltThreadFactory.java
+++ src/main/java/com/xceptance/xlt/engine/XltThreadFactory.java
@@ -15,15 +15,9 @@
  */
 package com.xceptance.xlt.engine;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import org.apache.commons.lang3.SystemUtils;
-
-import com.xceptance.common.lang.ReflectionUtils;
-import com.xceptance.xlt.api.util.XltException;
 
 public class XltThreadFactory implements ThreadFactory
 {
@@ -57,37 +51,11 @@ public class XltThreadFactory implements ThreadFactory
     {
         final Thread thread;
 
-        if (useVirtualThreads && SystemUtils.IS_JAVA_21)
+        if (useVirtualThreads)
         {
             System.out.println("Creating virtual thread");
 
-            // This is what actually needs to be done here:
-            // thread = Thread.ofVirtual().inheritInheritableThreadLocals(false).unstarted(runnable);
-
-            // To make the above Java 21 code compile on Java 11, use reflection.
-            try
-            {
-                // 1. get the virtual thread builder
-                // final ThreadBuilder threadBuilder = Thread.ofVirtual();
-                final Object threadBuilder = ReflectionUtils.callStaticMethod(Thread.class, "ofVirtual");
-
-                // 2. set whether to inherit inheritable thread locals
-                // threadBuilder.inheritInheritableThreadLocals(inheritInheritableThreadLocals);
-                // ReflectionUtils.callMethod(threadBuilder, "inheritInheritableThreadLocals",
-                // inheritInheritableThreadLocals);
-                ReflectionUtils.getMethod(threadBuilder.getClass(), "inheritInheritableThreadLocals", boolean.class)
-                               .invoke(threadBuilder, inheritInheritableThreadLocals);
-
-                // 3. create the thread
-                // threadBuilder.unstarted(this::run);
-                // thread = (Thread) ReflectionUtils.callMethod(threadBuilder, "unstarted", runnable);
-                thread = (Thread) ReflectionUtils.getMethod(threadBuilder.getClass(), "unstarted", Runnable.class).invoke(threadBuilder,
-                                                                                                                          runnable);
-            }
-            catch (final SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-            {
-                throw new XltException("Failed to create virtual thread", e);
-            }
+            thread = Thread.ofVirtual().inheritInheritableThreadLocals(false).unstarted(runnable);
         }
         else
         {
