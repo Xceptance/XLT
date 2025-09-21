8fd36544652ca6a8ce7c14087f9291e406d3fe15
Rene Schwietzke <r.schwietzke@xceptance.com>
2025-09-21 14:23:25 +0200

    Added comment and license header

--- src/main/java/com/xceptance/xlt/report/mergerules/MergeRuleProcessor.java
+++ src/main/java/com/xceptance/xlt/report/mergerules/MergeRuleProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2005-2025 Xceptance Software Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.xceptance.xlt.report.mergerules;
 
 import java.util.List;
@@ -7,6 +22,14 @@ import org.apache.commons.logging.LogFactory;
 
 import com.xceptance.xlt.api.engine.RequestData;
 
+/**
+ * This class processes requests according to a list of request processing rules. 
+ * The rules are applied in the order of their IDs. Skipping is possible, but jumping back is not.
+ * 
+ * @author Jörg Werner (Xceptance Software Technologies GmbH)
+ * @author Hartmut Arlt (Xceptance Software Technologies GmbH)
+ * @author Rene Schwietzke (Xceptance Software Technologies GmbH)
+ */
 public class MergeRuleProcessor
 {
     /**
@@ -50,15 +73,10 @@ public class MergeRuleProcessor
     
     /**
      * Processes a request according to the configured request processing rules. 
-     * Currently, this means renaming or discarding requests. Because we want to test it
-     * we made it static public. This is not meant to be a public API, but rather a utility method.
+     * Currently, this means renaming or discarding requests.
      *
      * @param requestData
      *            the request data record
-     * @param requestProcessingRules
-     *            the rules to apply
-     * @param removeIndexesFromRequestNames
-     *            in case we want to clean the name too
      * @return the processed request data record, or <code>null</code> if the data record is to be discarded
      */
     public RequestData postprocess(final RequestData requestData)
