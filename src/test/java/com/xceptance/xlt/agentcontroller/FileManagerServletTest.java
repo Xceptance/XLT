680dd5ad65565ecb142a68188c42ab822aff8b22
Robert Baumgarten <r.baumgarten@xceptance.net>
2025-12-08 17:07:15 +0100

    Address review feedback: Replace AssertionError with Assert.fail

--- src/test/java/com/xceptance/xlt/agentcontroller/FileManagerServletTest.java
+++ src/test/java/com/xceptance/xlt/agentcontroller/FileManagerServletTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2005-2025 Xceptance Software Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.xceptance.xlt.agentcontroller;
 
 import static org.mockito.Mockito.mock;
@@ -14,6 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -131,7 +147,7 @@ public class FileManagerServletTest
             // out = new FileOutputStream(file);
             // IOUtils.copy(in, out);
             // This definitely creates the file.
-            throw new AssertionError("File should have been created");
+            Assert.fail("File should have been created");
         }
     }
 
@@ -198,7 +214,7 @@ public class FileManagerServletTest
         // Verify file was created (even if empty)
         if (!targetFile.exists())
         {
-            throw new AssertionError("File should have been created");
+            Assert.fail("File should have been created");
         }
     }
 
