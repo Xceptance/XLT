b3afe47e921fff9c56c6a18e776cb1d905ec3a47
Rene Schwietzke <r.schwietzke@xceptance.com>
2026-09-02 14:04:17 +0200

    Add tests reproducing Groovy evaluation failure on merge rules with regex patterns

---
+++ src/test/java/com/xceptance/xlt/report/ReportGeneratorConfiguration_MergeRules_Test.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2005-2026 Xceptance Software Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xceptance.xlt.report;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.xceptance.xlt.api.engine.RequestData;
+import com.xceptance.xlt.report.mergerules.MergeRule;
+
+public class ReportGeneratorConfiguration_MergeRules_Test extends ReportGeneratorConfigurationTestBase
+{
+    private static final String PROP_MERGE_RULES_PREFIX = "com.xceptance.xlt.reportgenerator.requestMergeRules.";
+
+    @Test
+    public void getMergeRules_noRules()
+    {
+        final List<MergeRule> mergeRules = readReportGeneratorProperties().getMergeRules();
+        assertEquals(0, mergeRules.size());
+    }
+
+    @Test
+    public void getMergeRules_urlPatternWithHashRegex()
+    {
+        final String rulePrefix = PROP_MERGE_RULES_PREFIX + "1.";
+        appendPropertyToFile(rulePrefix + "newName", "{n} Cart");
+        appendPropertyToFile(rulePrefix + "urlPattern", "http://example.com/.*#{.*}");
+        appendPropertyToFile(rulePrefix + "stopOnMatch", "false");
+
+        final List<MergeRule> mergeRules = readReportGeneratorProperties().getMergeRules();
+        assertEquals(1, mergeRules.size());
+
+        final MergeRule rule = mergeRules.get(0);
+        assertNotNull(rule);
+
+        final RequestData data = new RequestData("MyRequest");
+        data.setUrl("http://example.com/item#abc");
+        rule.process(data);
+        assertEquals("MyRequest Cart", data.getName());
+    }
+
+    @Test
+    public void getMergeRules_namePatternWithHashRegex()
+    {
+        final String rulePrefix = PROP_MERGE_RULES_PREFIX + "1.";
+        appendPropertyToFile(rulePrefix + "newName", "MatchedName");
+        appendPropertyToFile(rulePrefix + "namePattern", "#{.*}");
+        appendPropertyToFile(rulePrefix + "stopOnMatch", "true");
+
+        final List<MergeRule> mergeRules = readReportGeneratorProperties().getMergeRules();
+        assertEquals(1, mergeRules.size());
+
+        final MergeRule rule = mergeRules.get(0);
+        assertNotNull(rule);
+
+        final RequestData data = new RequestData("#test");
+        rule.process(data);
+        assertEquals("MatchedName", data.getName());
+    }
+
+    @Test
+    public void getMergeRules_quantifierOnHashCharacter()
+    {
+        final String rulePrefix = PROP_MERGE_RULES_PREFIX + "1.";
+        appendPropertyToFile(rulePrefix + "newName", "MatchedHash");
+        appendPropertyToFile(rulePrefix + "urlPattern", ".*/page#{1,3}");
+        appendPropertyToFile(rulePrefix + "stopOnMatch", "true");
+
+        final List<MergeRule> mergeRules = readReportGeneratorProperties().getMergeRules();
+        assertEquals(1, mergeRules.size());
+
+        final MergeRule rule = mergeRules.get(0);
+        assertNotNull(rule);
+
+        final RequestData data = new RequestData("MyRequest");
+        data.setUrl("http://example.com/page##");
+        rule.process(data);
+        assertEquals("MatchedHash", data.getName());
+    }
+}
